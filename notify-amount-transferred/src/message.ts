148d0d4a94077b1693a03f7b579cf53d9e2172ba
Kenji Shimizu <jrywm121@gmail.com>
2022-02-24 00:36:06 +0900

    Add ceiling amout transferred by person

--- notify-amount-transferred/src/message.ts
+++ notify-amount-transferred/src/message.ts
@@ -7,15 +7,13 @@ export class DefaultNotificationMessage implements NotificationMessage {
   private subject: string;
   private body: string;
 
-  constructor(billing: number, housingCost: number) {
-    const amountPerCapita = Math.ceil((housingCost + billing) / 2).toLocaleString();
-
-    this.subject = `Amount to be transferred to the common account this month: ${amountPerCapita} yen`;
+  constructor(housingCost: number, billing: number, amountPerCapita: number) {
+    this.subject = `Amount to be transferred to the common account this month: ${amountPerCapita.toLocaleString()} yen`;
     this.body = `
-- Rent: ${housingCost} yen
-- Shared credit card billing: ${billing} yen
+- Rent: ${housingCost.toLocaleString()} yen
+- Shared credit card billing: ${billing.toLocaleString()} yen
 
-Please transfer ${amountPerCapita} yen per person to the common account.
+Please transfer ${amountPerCapita.toLocaleString()} yen per person to the common account.
 `;
   }
 
